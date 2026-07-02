bc87791b48951a129f7e01193842e9149d3ec9eb
Desai <fodbog@gmail.com>
2026-07-02 22:26:35 +0300

    filling torrentfile struct

--- internal/torrentfile/torrentfile.go
+++ internal/torrentfile/torrentfile.go
@@ -11,7 +11,7 @@ type TorrentFile struct {
 	Announce    string
 	InfoHash    [20]byte
 	PieceHashes [][20]byte
-	PieceLength int
+	PieceLength int64
 	Length      int64
 	Name        string
 }
@@ -32,7 +32,7 @@ func Open(path string) (*TorrentFile, error) {
 
 	d := bencode.NewDecoder(data)
 
-	_, err = d.Decode()
+	raw, err := d.Decode()
 	if err != nil {
 		return nil, fmt.Errorf("unable to decode file: %w", err)
 	}
@@ -40,5 +40,57 @@ func Open(path string) (*TorrentFile, error) {
 	start, end := d.GetInfoRaw()
 	data = data[start:end]
 	hash := sha1.Sum(data)
-	return &TorrentFile{InfoHash: hash}, nil
+
+	top, ok := raw.(map[string]any)
+	if !ok {
+		return nil, fmt.Errorf("cannot finish type assertion")
+	}
+
+	announce, ok := top["announce"].(string)
+	if !ok {
+		return nil, fmt.Errorf("cannot finish type assertion")
+	}
+
+	info, ok := top["info"].(map[string]any)
+	if !ok {
+		return nil, fmt.Errorf("cannot finish type assertion")
+	}
+
+	name, ok := info["name"].(string)
+	if !ok {
+		return nil, fmt.Errorf("cannot finish type assertion")
+	}
+
+	length, ok := info["length"].(int64)
+	if !ok {
+		return nil, fmt.Errorf("cannot finish type assertion")
+	}
+
+	pieceLength, ok := info["piece length"].(int64)
+	if !ok {
+		return nil, fmt.Errorf("cannot finish type assertion")
+	}
+
+	pieces, ok := info["pieces"].(string)
+	if !ok {
+		return nil, fmt.Errorf("missing or invalid 'pieces' field in info")
+	}
+	if len(pieces)%20 != 0 {
+		return nil, fmt.Errorf("corrupted file")
+	}
+
+	numPieces := len(pieces) / 20
+	pieceHashes := make([][20]byte, numPieces)
+	for i := 0; i < numPieces; i++ {
+		copy(pieceHashes[i][:], pieces[i*20:(i+1)*20])
+	}
+
+	return &TorrentFile{
+		InfoHash:    hash,
+		Announce:    announce,
+		Name:        name,
+		Length:      length,
+		PieceLength: pieceLength,
+		PieceHashes: pieceHashes,
+	}, nil
 }

4aef1ffd9521e16b2e37fbe9a691f78ed9af2ce1
Desai <fodbog@gmail.com>
2026-07-05 17:58:11 +0300

    tracker functionality

--- cmd/torrentd/main.go
+++ cmd/torrentd/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"torrentd/internal/torrentfile"
+	"torrentd/internal/tracker"
+)
+
 func main() {
+	peerID, err := tracker.GeneratePeerID()
+	if err != nil {
+		log.Fatal("failed to generate peer id: ", err)
+	}
+
+	if len(os.Args) < 2 {
+		log.Fatal("usage: torrentd <file.torrent>")
+	}
+	path := os.Args[1]
+	tf, err := torrentfile.Open(path)
+	if err != nil {
+		log.Fatal("failed to open torrentfile: ", err)
+	}
+
+	fmt.Println(tracker.Announce(tf, peerID, 6881))
 
 }

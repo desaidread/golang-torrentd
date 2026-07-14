a2119e903df53629f8a2e045e2fee75b78c19c8e
Desai <fodbog@gmail.com>
2026-07-14 16:44:58 +0300

    Message serializer and messageReader

--- internal/peer/message.go
+++ internal/peer/message.go
@@ -1,1 +1,59 @@
 package peer
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+type MessageID uint8
+
+const (
+	MsgChoke         MessageID = 0
+	MsgUnchoke       MessageID = 1
+	MsgInterested    MessageID = 2
+	MsgNotInterested MessageID = 3
+	MsgHave          MessageID = 4
+	MsgBitfield      MessageID = 5
+	MsgRequest       MessageID = 6
+	MsgPiece         MessageID = 7
+	MsgCancel        MessageID = 8
+)
+
+type Message struct {
+	ID      MessageID
+	Payload []byte
+}
+
+func (m *Message) Serialize() []byte {
+	length := uint32(len(m.Payload) + 1)
+	buf := make([]byte, 4+length)
+	binary.BigEndian.PutUint32(buf[0:4], length)
+	buf[4] = byte(m.ID)
+	copy(buf[5:], m.Payload)
+	return buf
+}
+
+func ReadMessage(r io.Reader) (*Message, error) {
+	lengthBuf := make([]byte, 4)
+	_, err := io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	length := binary.BigEndian.Uint32(lengthBuf)
+
+	//keep alive - просто п одерживаем соединение, возвращаем nil, nil
+	if length == 0 {
+		return nil, nil
+	}
+
+	messageBuf := make([]byte, length)
+	_, err = io.ReadFull(r, messageBuf)
+	if err != nil {
+		return nil, err
+	}
+	return &Message{
+		ID:      MessageID(messageBuf[0]),
+		Payload: messageBuf[1:],
+	}, nil
+}
